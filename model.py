bdfde49517f677d32561927a03e64bd00cc80c21
yus <644266701@qq.com>
2022-11-14 18:48:28 +0800

    update data analysis 1114

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
 from sktime.forecasting.fbprophet import Prophet
+
+Process: 193220
+ExecStart = / usr / local / bin / k3s
+server - -docker - -write - kubeconfig / home / yus /.kube / config - -write - kubeconfig - mode
+666(code=exited, status=1 / FAILURE)
+Main
+PID: 193220(code=exited, status=1 / FAILURE)
